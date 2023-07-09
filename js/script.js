e1adcce030f111915a0d4e307df15e06c96436ad
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-09 10:01:25 +0000

    fix hm-menu

--- js/script.js
+++ js/script.js
@@ -1,18 +1,7 @@
-(function($) {
-  var $nav   = $('#navArea');
-  var $btn   = $('.toggle_btn');
-  var $mask  = $('#mask');
-  var open   = 'open'; // class
-  // menu open close
-  $btn.on( 'click', function() {
-    if ( ! $nav.hasClass( open ) ) {
-      $nav.addClass( open );
-    } else {
-      $nav.removeClass( open );
-    }
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
   });
-  // mask close
-  $mask.on('click', function() {
-    $nav.removeClass( open );
-  });
-} )(jQuery);
+});
